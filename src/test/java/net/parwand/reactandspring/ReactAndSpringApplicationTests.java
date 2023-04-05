b7777df54f25ee066ddb94b560c0f88c135f056a
Parwand <parwand.alsino@hhu.de>
2023-04-05 11:44:44 +0200

    Initial commit: backend

---
+++ src/test/java/net/parwand/reactandspring/ReactAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.parwand.reactandspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactAndSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
