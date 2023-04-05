de13841e0369cfb0358a027d049f206e6f1f1dde
Parwand <parwand.alsino@hhu.de>
2023-04-05 12:28:50 +0200

    created API with axios to get Message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [message, setMessage] = useState('Default Message');
+
+  useEffect(() => {
+    getMessage();
+  });
+
+  const getMessage = async() => {
+    const response =  await axios.get("http://localhost:8080/api/hello");
+    setMessage(response)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <p>Message from Backend: {message}</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
